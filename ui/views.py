9b069fbfc81aae6538c163beb2420abcede2bfe5
njinoc <nicolejohn.inoc@cit.edu>
2025-10-08 16:03:28 +0800

    feat: Implement UI for dashboard, profile, and edit profile pages

--- ui/views.py
+++ ui/views.py
@@ -6,6 +6,19 @@ def login_view(request):
     return render(request, 'ui/auth/login.html')
 def signup_view(request):
     return render(request, 'ui/auth/signup.html')
-
-
-# Create your views here.
+def dashboard_view(request):
+    return render(request, 'ui/student/dashboard.html')
+def profile_view(request):
+    return render(request, 'ui/student/profile.html')
+def edit_profile_view(request):
+    return render(request, 'ui/student/edit_profile.html')
+def my_skill_view(request):
+    return render(request, 'ui/student/my_skills.html')
+def request_view(request):
+    return render(request, 'ui/student/request.html')
+def find_skill_view(request):
+    return render(request, 'ui/student/find_skills.html')
+def session_view(request):
+    return render(request, 'ui/student/session.html')
+def skill_view(request):
+    return render(request, 'ui/student/skill.html')
